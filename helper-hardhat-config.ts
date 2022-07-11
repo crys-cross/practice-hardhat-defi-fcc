29a14392900e0cebfffb80e23aa3d122546d2f95
CRYS <mariano.christopher123@gmail.com>
2022-07-11 18:46:22 +0800

    add interfaces and edit scripts

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -2,12 +2,15 @@ import { ethers } from "hardhat"
 
 export interface networkConfigItem {
     name?: string
-    vrfCoordinatorV2?: string
-    raffleEntranceFee?: any
-    gasLane?: string
-    subscriptionId?: string
-    callbackGasLimit?: string
-    keeperUpdateInterval?: string
+    wethToken?: string
+    lendingPoolAddressesProvider?: string
+
+    // vrfCoordinatorV2?: string
+    // raffleEntranceFee?: any
+    // gasLane?: string
+    // subscriptionId?: string
+    // callbackGasLimit?: string
+    // keeperUpdateInterval?: string
 }
 
 export interface networkConfigInfo {
@@ -19,33 +22,33 @@ export const networkConfig: networkConfigInfo = {
     // Default one is ETH/USD contract on Kovan
     31337: {
         name: "localhost",
+        wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        lendingPoolAddressesProvider: "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
         // vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        raffleEntranceFee: ethers.utils.parseEther("0.1"), // 0.1 ETH
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
-        subscriptionId: "6727",
-        callbackGasLimit: "500000" /*500,000*/,
-        keeperUpdateInterval: "30",
+        // raffleEntranceFee: ethers.utils.parseEther("0.1"), // 0.1 ETH
+        // gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        // subscriptionId: "6727",
+        // callbackGasLimit: "500000" /*500,000*/,
+        // keeperUpdateInterval: "30",
 
         // ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
     },
-    4: {
-        name: "rinkeby",
-        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        raffleEntranceFee: "100000000000000000", // 0.1 ETH
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subscriptionId: "6727", //for rinkeby
-        callbackGasLimit: "500000" /*500,000*/,
-        keeperUpdateInterval: "30",
+    // 4: {
+    //     name: "rinkeby",
+    //     vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    //     raffleEntranceFee: "100000000000000000", // 0.1 ETH
+    //     gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    //     subscriptionId: "6727", //for rinkeby
+    //     callbackGasLimit: "500000" /*500,000*/,
+    //     keeperUpdateInterval: "30",
 
-        // ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-    },
-    80001: {
-        name: "polygon(mumbai-testnet)",
-        // ethUsdPriceFeed: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
-    },
+    //     // ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+    // },
+    // 80001: {
+    //     name: "polygon(mumbai-testnet)",
+    //     // ethUsdPriceFeed: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
+    // },
 }
 
 export const developmentChains = ["hardhat", "localhost"]
-export const DECIMALS = 8
-export const INITIAL_ANSWER = 200000000000
 export const VERIFICATION_BLOCK_CONFIRMATIONS = 6
--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -1,5 +1,33 @@
+import { ethers, getNamedAccounts, network } from "hardhat"
+import { Address } from "hardhat-deploy/dist/types"
+import { networkConfig } from "../helper-hardhat-config"
+import { getWeth } from "./getWeth"
+import { ILendingPool } from "../typechain-types"
+
 const aaveBorrow = async () => {
     //the protocol treats everything as an ERC20 token
+    await getWeth()
+    const { deployer } = await getNamedAccounts()
+    //need abi and address to interact with aave protocol
+    //lendingpool address: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+    //lending pool ^
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool Address ${lendingPool.address}`)
+}
+
+export const getLendingPool = async (account: Address): Promise<ILendingPool> => {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        networkConfig[network.config.chainId!].lendingPoolAddressesProvider!,
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool: ILendingPool = await ethers.getContractAt(
+        "ILendingPool",
+        lendingPoolAddress,
+        account
+    )
+    return lendingPool
 }
 
 aaveBorrow()
